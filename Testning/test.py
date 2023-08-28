35be8fe3fe0ede5910095f1ad212334f3571701a
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-08-28 13:17:56 +0200

    Create test.py

---
+++ Testning/test.py
@@ -0,0 +1,66 @@
+from unittest import TestCase, main
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from os import path, getcwd
+
+
+
+class TestHemsida(TestCase):
+
+    # inställningar för hur testerna körs
+    stangintebrowsern = False  # om True så hålls webbläsaren öppen efter testerna är klara, annars stängs den
+    gomfonstret = False  # visar webbläsaren medan testerna körs
+
+    # setUpClass körs INNAN FÖRSTA testet
+    @classmethod
+    def setUpClass(cls):
+        chr_options = Options()
+
+        if cls.stangintebrowsern:
+            chr_options.add_experimental_option("detach", True)
+
+        if cls.gomfonstret:
+            chr_options.add_argument("--headless")
+
+        cls.browser = webdriver.Chrome(options=chr_options)
+
+    # tearDownClass körs EFTER SISTA testet
+    @classmethod
+    def tearDownClass(cls):
+        pass  # gör ingenting
+
+    # setUp körs INNAN VARJE TEST
+    def setUp(self):
+        pass  # gör ingenting
+
+    # tearDown körs EFTER VARJE TEST
+    def tearDown(self):
+        self.browser.get('about:blank')  # gå till en tom sida för att undvika att tidigare test påverkar senare
+
+
+    # HÄR BÖRJAR TESTERNA
+    def testPageText(self):
+        self.browser.get(path.join(getcwd(), "Estefans Hemsida", 'index.html'))
+        self.assertIn("Estefan", self.browser.page_source)
+        print("this is: "+ self.browser.title) #VARIABELN ÄR self.browser
+
+    def testPageText(self):
+        self.browser.get(path.join(getcwd(), "Estefans Hemsida", 'index.html'))
+        self.assertIn("Estefan", self.browser.page_source)
+        print("this is: "+ self.browser.title) #VARIABELN ÄR self.browser
+        
+        self.assertEqual("hej1", self.browser.title)
+
+        if self.browser.title == "hej":
+            print("COMPLETED: TITLE")
+        else:
+            print("FAIL: TITLE")
+
+    
+
+
+
+
+# denna bit finns här så att testerna körs om filen körs som vanligt python-program
+if __name__ == '__main__':
+    main(verbosity=2)
