d1003963a94d51324af08ded80be564038e2cc59
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-08-28 14:33:07 +0200

    Kontakt och hitta oss navbar

--- Testning/test.py
+++ Testning/test.py
@@ -2,10 +2,13 @@ from unittest import TestCase, main
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from os import path, getcwd
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class TestHemsida(TestCase):
+
+
 
     # inställningar för hur testerna körs
     stangintebrowsern = False  # om True så hålls webbläsaren öppen efter testerna är klara, annars stängs den
@@ -42,8 +45,27 @@ class TestHemsida(TestCase):
 
     def testPageText(self):
         self.browser.get(path.join(getcwd(), 'index.html'))
-        self.assertEqual("hej1", self.browser.title)
+        self.assertEqual("Frisör Saxé", self.browser.title)
 
+    def testnavbar(self):
+        self.browser.get(path.join(getcwd(), 'index.html'))
+        self.assertIn("Hitta oss", self.browser.page_source)
+        self.assertIn("Kontakt", self.browser.page_source)
+        self.assertIn("Frisör Saxé", self.browser.page_source)
+    
+    def testknapp_kontakt(self):
+        self.browser.get(path.join(getcwd(), 'index.html'))
+        self.browser.find_element(By.LINK_TEXT, "Kontakt").click()
+
+        self.assertIn("kol", self.browser.page_source)
+
+    def testknapp_hittaoss(self):
+        self.browser.get(path.join(getcwd(), 'index.html'))
+        self.browser.find_element(By.LINK_TEXT, "Hitta oss").click()
+
+        self.assertIn("polen", self.browser.page_source)
+
+        
     
 
 
@@ -52,3 +74,5 @@ class TestHemsida(TestCase):
 # denna bit finns här så att testerna körs om filen körs som vanligt python-program
 if __name__ == '__main__':
     main(verbosity=2)
+
+
