36c2a5e2561ca265f0ed1ada5511909f9dcdfc1d
Aprizotec <65119813+Johannes29@users.noreply.github.com>
2023-08-29 10:16:56 +0200

    Working validation for html and css files

--- validate.py
+++ validate.py
@@ -1,27 +1,57 @@
-# importing the requests library
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 
-# api-endpoint
 URL = "https://validator.w3.org/nu/"
 
-file_contents = ""
+chrome_options = Options()
+# Prevents chrome from closing when done, from https://stackoverflow.com/a/51865955
+chrome_options.add_experimental_option("detach", True)
+browser = webdriver.Chrome(options=chrome_options)
 
-with open("index.html", "r") as file:
-    file_contents += file.read()
+# Get all file paths
+paths = []
+import os
+for root, dirs, files in os.walk(".", topdown=False):
+   for name in files:
+        paths.append(os.path.join(root, name))
+   for name in dirs:
+        paths.append(os.path.join(root, name))
 
-s = requests.session()
+tabs_open = 1
 
-s.headers = {
-    "Accept": "text/html",
-    "Content-Type": "text/html"
-}
+# Loop through file paths and validate the files
+for path in paths:
+    mime_type = ""
+    if path.endswith("_error.html"):
+        continue
+    if path.endswith(".html"):
+        mime_type = "text/html"
+    elif path.endswith(".css"):
+        mime_type = "text/css"
+    else:
+        continue
 
-response = s.post(URL, data=file_contents)
+    # Open a new tab and switch to it
+    browser.execute_script("window.open('');")
+    tabs_open += 1
+    browser.switch_to.window(browser.window_handles[tabs_open - 1])
+    # Open the validation website
+    browser.get(URL)
+    # Change dropdown to "file upload"
+    docselector = browser.find_element(By.ID, "docselect")
+    select = Select(docselector)
+    select.select_by_visible_text("file upload")
+    # Upload and submit file
+    file_upload_button = browser.find_element(By.ID, "doc")
+    file_upload_button.send_keys(os.path.abspath(path))
+    check_button = browser.find_element(By.ID, "submit")
+    check_button.click()
 
-if "<div id=\"results\"><p class=\"success\">" in response.text:
-    print("success")
-else:
-    print("fail")
+    # Check if the uploaded file passed the validation
+    if "<div id=\"results\"><p class=\"success\">" in browser.page_source:
+        print(path, "passed")
+    else:
+        print(path, "failed")
 
-with open("output.html", "w", encoding="utf-8") as file:
-    file.write(response.text)
