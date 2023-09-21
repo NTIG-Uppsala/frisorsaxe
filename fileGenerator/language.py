fdf07bebe7dfe6f2e8f4fe5bb2a29d469061545d
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-09-21 11:32:43 +0200

    loop through all lang on translater

--- fileGenerator/language.py
+++ fileGenerator/language.py
@@ -1,21 +1,37 @@
 # Step 1: Read and parse the HTML file
-with open("template.html", "r", encoding="utf-8") as html_file:
+with open("fileGenerator/template.html", "r", encoding="utf-8") as html_file:
     html_content = html_file.read()
 
+outputNumber = 0
+
+languages = [
+    "fileGenerator/kirunaeng.txt",
+    "fileGenerator/kirunaswe.txt",
+    "fileGenerator/luleåeng.txt",
+    "fileGenerator/luleåswe.txt",
+]
+output = [
+    "testkirunaeng.html",
+    "testkirunaswe.html",
+    "testluleaeng.html",
+    "testluleaswe.html",
+]
 # Step 2: Read the text document with variable replacements
-with open("Kirunaeng.txt", "r", encoding="utf-8") as variables_file:
-    variables = {}
-    for line in variables_file:
-        key, value = line.strip().split("|")
-        variables[key] = value
+for language in languages:
+    with open(language, "r", encoding="utf-8") as variables_file:
+        variables = {}
+        for line in variables_file:
+            key, value = line.strip().split("|")
+            variables[key] = value
 
-# Step 3: Replace variables in the HTML content
-for variable, value in variables.items():
-    html_content = html_content.replace(f"^{variable}^", value)
-    html_content = html_content.replace(f"*{variable}*", value)
+    # Step 3: Replace variables in the HTML content
+    for variable, value in variables.items():
+        html_content = html_content.replace(f"^{variable}^", value)
+        html_content = html_content.replace(f"*{variable}*", value)
 
-# Step 4: Write the modified HTML content to a new HTML file
-with open("output.html", "w", encoding="utf-8") as output_file:
-    output_file.write(html_content)
+    # Step 4: Write the modified HTML content to a new HTML file
+    with open(output[i], "w", encoding="utf-8") as output_file:
+        output_file.write(html_content)
+    outputNumber += 1
 
 print("HTML file with variables replaced has been generated as 'output.html'")
