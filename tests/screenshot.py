613d6118891218a60cf6445338e222ec47fe866d
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-09-26 11:05:28 +0200

    Update screenshot.py

--- tests/screenshot.py
+++ tests/screenshot.py
@@ -32,7 +32,7 @@ class screenshotNoJs(TestCase):
         pass
 
     def setUp(self):
-        self.browser.get(path.join(getcwd(), "kirunaswe.html"))
+        self.browser.get(path.join(getcwd(), "subPages/kirunaswe.html"))
 
     def tearDown(self):
         self.browser.get("about:blank")
