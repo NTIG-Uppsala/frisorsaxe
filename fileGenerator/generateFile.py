fecbeaded0bcaed44cd1c4d129a40151f3d73abe
Leo Ahlinder <ahlinder.leo@icloud.com>
2023-09-28 09:23:05 +0200

    camelcase variables in filegenerator

--- fileGenerator/generateFile.py
+++ fileGenerator/generateFile.py
@@ -5,7 +5,7 @@ translations = json.load(open("fileGenerator/translations.json"))
 
 
 # Function to replace placeholders in the HTML template
-def replace_placeholders(html_template, language, location):
+def replacePlaceholders(htmlTemplate, language, location):
     # Get the translation for the selected language and location
     translation = translations.get(language, {}).get(location, {})
 
@@ -14,38 +14,38 @@ def replace_placeholders(html_template, language, location):
         opendays = "\n".join(
             [f"<p>{value}</p>" for value in translation["OPENDAYS"].values()]
         )
-        html_template = html_template.replace("*OPENDAYS*", opendays)
+        htmlTemplate = htmlTemplate.replace("*OPENDAYS*", opendays)
 
     # Replace placeholders in the HTML template with values from OPENDAYSTIME
     if "OPENDAYSTIME" in translation:
-        opendaystime = "\n".join(
+        openDaysTime = "\n".join(
             [f"<p>{value}</p>" for value in translation["OPENDAYSTIME"].values()]
         )
-        html_template = html_template.replace("*OPENDAYSTIME*", opendaystime)
+        htmlTemplate = htmlTemplate.replace("*OPENDAYSTIME*", openDaysTime)
 
     # Replace placeholders in the HTML template
     for key, value in translation.items():
         placeholder = f"*{key.upper()}*"
-        html_template = html_template.replace(
+        htmlTemplate = htmlTemplate.replace(
             placeholder, str(value)
         )  # Ensure the value is converted to a string
 
-    return html_template
+    return htmlTemplate
 
 
 # Example usage
-selected_language = ["eng", "swe", "fin"]  # Change this to the desired language
-selected_location = ["lulea", "kiruna"]  # Change this to the desired location
-for language in selected_language:
-    for location in selected_location:
-        html_template = open("fileGenerator/template.html", "r").read()
-        translated_html = replace_placeholders(html_template, language, location)
+selectedLanguage = ["eng", "swe", "fin"]  # Change this to the desired language
+selectedLocation = ["lulea", "kiruna"]  # Change this to the desired location
+for language in selectedLanguage:
+    for location in selectedLocation:
+        htmlTemplate = open("fileGenerator/template.html", "r").read()
+        translatedHtml = replacePlaceholders(htmlTemplate, language, location)
 
         # Define the file name for the translated HTML
-        translated_file_name = location + language + ".html"
+        translatedFileName = location + language + ".html"
 
         # Create the translated HTML file and write the content
-        with open(translated_file_name, "w") as translated_file:
-            translated_file.write(translated_html)
+        with open(translatedFileName, "w") as translatedFile:
+            translatedFile.write(translatedHtml)
 
-        print(f"Translated HTML saved to '{translated_file_name}'")
+        print(f"Translated HTML saved to '{translatedFileName}'")
