a2164332234da5cbe22cb959f3f7b91c598317c9
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-10-03 14:43:14 +0200

    tempo

--- fileGenerator/findLangselectedlang/pot.py
+++ fileGenerator/findLangselectedlang/pot.py
@@ -1,4 +1,11 @@
 import json
 
+jsonFile = json.load(open("fileGenerator/findLangselectedlang/data.json"))
+
 selectedLanguage = ["en", "sv", "fi"]  # Change this to the desired language
-selectedLocation = ["lulea", "kiruna"]  # Change this to the desired location
+selectedLocation = ["LULEA", "KIRUNA"]
+
+
+location_names = list(jsonFile["Locations"].keys())
+for location in location_names:
+    print(location)
